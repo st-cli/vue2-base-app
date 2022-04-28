b6e5678c86252b762a93caf58e39e80620f8ab0e
houyueke <442067356@qq.com>
2022-04-28 12:00:33 +0800

    feat: 修改主题配置方案

--- vue.config.js
+++ vue.config.js
@@ -3,45 +3,13 @@
  * @Autor: houyueke
  * @Date: 2022-04-01 10:38:46
  * @LastEditors: houyueke
- * @LastEditTime: 2022-04-22 14:00:50
+ * @LastEditTime: 2022-04-28 11:59:56
  */
 const CompressionPlugin = require('compression-webpack-plugin')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const {
-  themeColor,
-  linkColor,
-  successColor,
-  warningColor,
-  errorColor,
-  fontSizeBase,
-  headingColor,
-  textColor,
-  textColorSecondary,
-  disabledColor,
-  borderRadiusBase,
-  borderColorBase,
-  boxShadowBase
-} = require('./src/config/index')
-
-function modifyVars() {
-  return {
-    'primary-color': themeColor, // 全局主色
-    'link-color': linkColor, // 链接色
-    'success-color': successColor, // 成功色
-    'warning-color': warningColor, // 警告色
-    'error-color': errorColor, // 错误色
-    'font-size-base': fontSizeBase, // 主字号
-    'heading-color': headingColor, // 标题色
-    'text-color': textColor, // 主文本色
-    'text-color-secondary': textColorSecondary, // 次文本色
-    'disabled-color': disabledColor, // 失效色
-    'border-radius-base': borderRadiusBase, // 组件/浮层圆角
-    'border-color-base': borderColorBase, // 边框色
-    'box-shadow-base': boxShadowBase // 浮层阴影
-  }
-}
+const { modifyVars } = require('./src/config/index')
 
 module.exports = {
   lintOnSave: false, //保存时lint
